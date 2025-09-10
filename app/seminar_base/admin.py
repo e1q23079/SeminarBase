6dd06b1ec56bca9077aa7c3292715cd84900002f
e1q23079 <e1q23079@oit.ac.jp>
2025-09-10 17:01:29 +0900

    管理画面 表示文字変更

--- app/seminar_base/admin.py
+++ app/seminar_base/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 admin.site.register(Article)
+
+admin.site.site_header = "SeminarBase管理画面"
+admin.site.site_title = "SeminarBase管理画面"
+admin.site.index_title = "SeminarBase管理"
