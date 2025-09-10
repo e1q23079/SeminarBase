bdb52c8e044e964eb731111b05f67e966971f5e7
e1q23079 <e1q23079@oit.ac.jp>
2025-09-10 17:17:19 +0900

    トップページに記事一覧表示

--- app/seminar_base/views.py
+++ app/seminar_base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from markdownx.utils import markdownify
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'seminar_base/index.html')
+    articles = Article.objects.all()
+    return render(request, 'seminar_base/index.html', {'articles': articles})
 
 def article_detail(request, article_id):
-    from .models import Article
+    
     article = Article.objects.get(id=article_id)
     article.content = markdownify(article.content)
     return render(request, 'seminar_base/article_detail.html', {'article': article})
